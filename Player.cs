b0fe0c5651270287d22dec30e5d8219316424883
Rashi Parteti <117937829+rashiparteti@users.noreply.github.com>
2024-04-18 16:34:25 +0530

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, Damage
 
 {
     public float maxHealth = 100f;
